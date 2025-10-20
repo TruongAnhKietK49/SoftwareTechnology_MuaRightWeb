c828f38ffdd80da310599efae1379ccaf4eb2f57
Trần Khánh <trankhanh0525@gmail.com>
2025-10-20 19:09:45 +0700

    Add User Cart Function

--- sql-backend/server.js
+++ sql-backend/server.js
@@ -18,6 +18,12 @@ const signUpRoutes = require("./routes/signUpRoute")
 const signInRoutes = require("./routes/signInRoute")
 app.use('/api', signUpRoutes, signInRoutes)
 
+const cartRoutes = require("./routes/cartRoutes");
+app.use("/cart", cartRoutes);
+
+const productUserRoutes = require("./routes/productRoutes");
+app.use("/products", productUserRoutes);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server đang chạy trên cổng ${PORT}`);
