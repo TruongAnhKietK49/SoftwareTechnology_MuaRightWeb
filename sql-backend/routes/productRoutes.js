04e1e1515be63c15cd5ecafee5574526aaeaa8cb
Trần Khánh <trankhanh0525@gmail.com>
2025-10-23 09:22:29 +0700

    fix cartList, update review star

--- sql-backend/routes/productRoutes.js
+++ sql-backend/routes/productRoutes.js
@@ -28,4 +28,28 @@ router.get("/getProductList", async (req, res) => {
   }
 });
 
+router.get("/getProductReview", async (req, res) => {
+  try {
+    const pool = await getPool();
+    const request = pool.request();
+    const result = await request.query(`
+      SELECT * FROM Review;
+    `);
+    if (result) {
+      res.json({
+        data: result.recordset
+      });
+    }
+    else {
+      res.json({
+        success: false,
+        message: "Không thể lấy đánh giá sản phẩm.",
+      });
+    }
+  } catch (err) {
+    console.error("Error fetching product reviews:", err);
+    res.status(500).json({ success: false, message: "Lỗi server." });
+  }
+});
+
 module.exports = router;
