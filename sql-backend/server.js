442e2642b5dc9f49ad97ec539b628511fcfa5ebb
TruongAnhKietK49 <4901104078@student.hcmue.edu.vn>
2025-10-28 12:15:03 +0700

    Update Routes

--- sql-backend/server.js
+++ sql-backend/server.js
@@ -5,22 +5,28 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(cors());
 // app.use(bodyParser.json());
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ extended: true, limit: '20mb' }));
-
-
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
 const accountRoutes = require("./routes/Admin/accountRoute");
 const adminProfileRoutes = require("./routes/Admin/adminProfileRoute");
 const inventoryRoutes = require("./routes/Admin/inventoryRoute");
 const warehouseRoutes = require("./routes/Admin/warehouseRoute");
-const dashboardRoutes = require("./routes/Admin/dashboardRoute")
-app.use("/admin", adminProfileRoutes, accountRoutes, inventoryRoutes, warehouseRoutes, dashboardRoutes);
-const shipperRoutes = require("./routes/Shipper/shipperRoutes"); 
-app.use("/api/shipper", shipperRoutes);            
+const dashboardRoutes = require("./routes/Admin/dashboardRoute");
+app.use(
+  "/admin",
+  adminProfileRoutes,
+  accountRoutes,
+  inventoryRoutes,
+  warehouseRoutes,
+  dashboardRoutes
+);
+
+const shipperRoutes = require("./routes/Shipper/shipperRoutes");
+app.use("/api/shipper", shipperRoutes);
 const sellerDashboardRoutes = require("./routes/Seller/sellerDashboardRoute");
 const productRoutes = require("./routes/Seller/productRoute");
-const sellerOrderRoute = require('./routes/Seller/orderRoute');
+const sellerOrderRoute = require("./routes/Seller/orderRoute");
 const statisticRoutes = require("./routes/Seller/statisticRoute");
 const profileRoutes = require("./routes/Seller/profileRoute");
 app.use(
@@ -32,12 +38,20 @@ app.use(
   profileRoutes
 );
 
-const signUpRoutes = require("./routes/signUpRoute")
-const signInRoutes = require("./routes/signInRoute")
-app.use('/api', signUpRoutes, signInRoutes)
+const signUpRoutes = require("./routes/signUpRoute");
+const signInRoutes = require("./routes/signInRoute");
+app.use("/api", signUpRoutes, signInRoutes);
+
+const cartRoutes = require("./routes/cartRoutes");
+app.use("/cart", cartRoutes);
+
+const productUserRoutes = require("./routes/productRoutes");
+app.use("/products", productUserRoutes);
+
+const userProfileRoutes = require("./routes/userRoutes");
+app.use("/user", userProfileRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server đang chạy trên cổng ${PORT}`);
 });
-
